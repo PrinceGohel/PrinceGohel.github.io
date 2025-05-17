9cef2c2521c875ac76e2406c30c7c53bfb8949d5
Prince Gohel <princegohel.dev@gmail.com>
2025-05-17 18:56:57 +0530

    V 0.5
    some major design changes

--- main.js
+++ main.js
@@ -3,17 +3,7 @@ function toggleMenu() {
   const navLinks = document.getElementById('nav-links');
   navLinks.classList.toggle('show');
   }
-
-// Scroll to Top button
-const scrollBtn = document.getElementById("scrollTopBtn");
-
-window.onscroll = function() {
-// Show button after scrolling 300px
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    scrollBtn.style.display = "block";
-  } else {
-    scrollBtn.style.display = "none";
-    }
+  
 // Animate sections on scroll
     const sections = document.querySelectorAll('section');
     sections.forEach(section => {
@@ -22,9 +12,3 @@ window.onscroll = function() {
         section.classList.add('show');
       }
     });
-  };
-  
-function scrollToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-  }
